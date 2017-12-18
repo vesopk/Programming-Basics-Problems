2d0d1eff9c01db63251f214b30ff2ecf1c68733a
vesopk <vesopk979@gmail.com>
2017-12-19 00:40:25 +0200

    adding some code!

--- Exam17Dec/FamilyIvanovi/Program.cs
+++ Exam17Dec/FamilyIvanovi/Program.cs
@@ -6,7 +6,16 @@ namespace FamilyIvanovi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var madamIvanovaMoney = decimal.Parse(Console.ReadLine());
+            var firstChildPresentPrice = decimal.Parse(Console.ReadLine());
+            var secondChildPresentPrice = decimal.Parse(Console.ReadLine());
+            var thirdChildPresentPrice = decimal.Parse(Console.ReadLine());
+
+            var donation = madamIvanovaMoney - firstChildPresentPrice - secondChildPresentPrice -
+                           thirdChildPresentPrice;
+            donation *= 0.9M;
+
+            Console.WriteLine($"{donation:F2}");
         }
     }
 }
